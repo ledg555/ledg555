5a36adc6bc8348e588a209ecdf516ae888f83a5a
Luis Delgado <dev.ledg555@gmail.com>
2024-11-05 14:59:00 -0600

    feat: add i18n and core translations

---
+++ src/i18n.ts
@@ -0,0 +1,9 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n.use(initReactI18next).use(LanguageDetector).init({
+  fallbackLng: "en",
+});
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./router/Router";
+import "./i18n";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
