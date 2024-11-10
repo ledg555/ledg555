84e23677df8e8a25e64fe737443e4225079445df
Luis Delgado <dev.ledg555@gmail.com>
2024-11-10 14:31:33 -0600

    feat: more translation keys, header and main section of home page

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import { Accordion, AccordionTab } from "primereact/accordion";
+
+export default function Main() {
+  return (
+    <Accordion>
+      <AccordionTab header="hola">
+        <p>Adiós</p>
+      </AccordionTab>
+    </Accordion>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./router/Router";
 import "./i18n";
+import { PrimeReactProvider } from "primereact/api";
+import Tailwind from "primereact/passthrough/tailwind";
 import "./index.css";
+import "primeicons/primeicons.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router/>
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      <Router />
+    </PrimeReactProvider>
   </StrictMode>
 );
