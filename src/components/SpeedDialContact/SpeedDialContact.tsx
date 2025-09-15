a7a9e5e8cf7d08e15c30c97579f5133ef7830977
ledg555 <dev.ledg555@gmail.com>
2025-09-14 21:18:03 -0600

    feat: speed dial nav

--- src/components/SpeedDialContact/SpeedDialContact.tsx
+++ src/components/SpeedDialContact/SpeedDialContact.tsx
@@ -2,20 +2,9 @@ import { useTranslation } from "react-i18next";
 import { Tooltip } from "primereact/tooltip";
 import { speedDialContactIcons } from "./speed-dial-contact-config";
 import { HiOutlineChatBubbleLeft } from "react-icons/hi2";
-import { useEffect, useState } from "react";
 
 export function SpeedDialContact() {
   const { t } = useTranslation();
-  const [showMainIcon, setShowMainIcon] = useState(false);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setShowMainIcon((prev) => !prev);
-    }, 4000);
-
-    // Cleanup interval on component unmount
-    return () => clearInterval(interval);
-  }, []);
 
   const speedDialContactItems = t("contactData", { ns: "ui" }).map(
     (item: {
@@ -28,9 +17,9 @@ export function SpeedDialContact() {
       return (
         <button
           key={item.key}
-          className="bg-green-400 rounded-full p-2 cursor-pointer hover:scale-110 duration-150 ease-in"
+          className="bg-green-400 rounded-full p-2.5 cursor-pointer hover:scale-110 duration-100 ease-in"
         >
-          {<Icon size={24} />}
+          {<Icon size={20} />}
         </button>
       );
     }
@@ -42,14 +31,9 @@ export function SpeedDialContact() {
         position="left"
       />
       <button className="relative h-12 aspect-square p-0 rounded-full bg-blue-500 perspective-dramatic">
-        {/* <div className="absolute top-3 left-3 w-1/2 aspect-square rounded-full animate-ping bg-green-300"></div> */}
-        <div
-          className={`absolute top-0 left-0 w-full aspect-square rounded-full transform-3d transition-transform duration-[1000ms] hover:animate-contact-button ${
-            showMainIcon ? "rotate-y-360" : "rotate-y-180"
-          }`}
-        >
-          <div className="flex justify-center items-center aspect-square rounded-full p-2 bg-blue-500 rotate-y-180 border-3 border-black backface-hidden">
-            <HiOutlineChatBubbleLeft size={24} className="animate-pulse" />
+        <div className="absolute top-0 left-0 w-full aspect-square rounded-full transform-3d animate-flip-y hover:scale-110 duration-150">
+          <div className="flex justify-center items-center aspect-square rounded-full p-2 rotate-y-180 border-3 border-black backface-hidden">
+            <HiOutlineChatBubbleLeft size={24} />
           </div>
           <img
             src="/Luis Delgado.webp"
@@ -57,7 +41,7 @@ export function SpeedDialContact() {
             className="absolute top-0 left-0 block w-full rounded-full border-3 border-black backface-hidden"
           />
         </div>
-        <div className="absolute flex flex-col justify-between items-center gap-1.5 top-14 left-1 bg-sky-400">
+        <div className="absolute flex flex-col justify-between items-center gap-1.5 top-14 left-1">
           {speedDialContactItems}
         </div>
       </button>
---
+++ src/components/SpeedDialNav/SpeedDialNav.tsx
@@ -0,0 +1,42 @@
+import { useTranslation } from "react-i18next";
+import { Tooltip } from "primereact/tooltip";
+import { HiOutlinePlus } from "react-icons/hi2";
+import { useState } from "react";
+import { Link } from "react-router";
+import { navLinks } from "../Header/nav-config";
+
+export function SpeedDialNav() {
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
+
+  const speedDialContactItems = navLinks.map((item) => (
+    <Link
+      title={t(`navigationData.${item.translationKey}`, { ns: "ui" })}
+      key={item.translationKey}
+      to={`${item.url}`}
+      className="bg-green-400 rounded-full p-2.5 cursor-pointer hover:scale-110 duration-100 ease-in"
+    >
+      <item.icon size={20} />
+    </Link>
+  ));
+  return (
+    <>
+      <Tooltip target=".speeddial-nav .p-speeddial-action" position="left" />
+      <button
+        className="relative h-12 aspect-square p-0 rounded-full bg-blue-500 perspective-dramatic"
+        onClick={() => setOpen(!open)}
+      >
+        <div
+          className={`flex justify-center items-center aspect-square rounded-full p-2 border-3 border-black duration-500 ${
+            open && "-rotate-z-135"
+          }`}
+        >
+          <HiOutlinePlus size={24} />
+        </div>
+        <div className="absolute flex flex-col justify-between items-center gap-1.5 top-14 left-1">
+          {speedDialContactItems}
+        </div>
+      </button>
+    </>
+  );
+}
