95fd0914de85a1b60db3b05108a09d1ee5cff7c8
ledg555 <dev.ledg555@gmail.com>
2025-09-16 02:13:02 -0600

    feat: mess with pull and press animations

--- src/components/SpeedDialNav/SpeedDialNav.tsx
+++ src/components/SpeedDialNav/SpeedDialNav.tsx
@@ -14,7 +14,9 @@ export function SpeedDialNav() {
       title={t(`navigationData.${item.translationKey}`, { ns: "ui" })}
       key={item.translationKey}
       to={`${item.url}`}
-      className="bg-green-400 rounded-full p-2.5 cursor-pointer hover:scale-110 duration-100 ease-in"
+      className={`bg-green-400 p-2.5 rounded-full cursor-pointer hover:scale-110 duration-100 ease-in ${
+        open && "animate-pull"
+      }`}
     >
       <item.icon size={20} />
     </Link>
@@ -23,17 +25,19 @@ export function SpeedDialNav() {
     <>
       <Tooltip target=".speeddial-nav .p-speeddial-action" position="left" />
       <button
-        className="relative h-12 aspect-square p-0 rounded-full bg-blue-500 perspective-dramatic"
+        className="block sm:hidden relative h-12 aspect-square p-0 rounded-full bg-blue-500 perspective-dramatic"
         onClick={() => setOpen(!open)}
       >
         <div
-          className={`flex justify-center items-center aspect-square rounded-full p-2 border-3 border-black duration-500 ${
+          className={`flex justify-center items-center aspect-square rounded-full p-2 border-3 border-black duration-500 hover:scale-110 animate-press ${
             open && "-rotate-z-135"
           }`}
         >
           <HiOutlinePlus size={24} />
         </div>
-        <div className="absolute flex flex-col justify-between items-center gap-1.5 top-14 left-1">
+        <div
+          className={`absolute flex-col justify-between items-center gap-1.5 top-14 left-1 flex`}
+        >
           {speedDialContactItems}
         </div>
       </button>
