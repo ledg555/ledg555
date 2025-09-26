ea797a23938e67530e8df69c83872338cc3df611
ledg555 <dev.ledg555@gmail.com>
2025-09-25 23:50:38 -0600

    feat: octagon-sm shape, switches bg gradient

--- src/components/LangSwitch.tsx
+++ src/components/LangSwitch.tsx
@@ -7,27 +7,25 @@ export default function LangSwitch() {
   const { isDarkTheme } = useStore();
   return (
     <>
-      <motion.button
+      <button
         onClick={() =>
           i18n.changeLanguage(i18n.language === "en" ? "es" : "en")
         }
         className={`
-        relative h-8 w-8 lg:w-14 rounded-full border-2 transition-all duration-300
+        relative h-8 w-8 lg:w-14 rounded-full border-2 hover:scale-105 transition-all duration-100
         ${
           isDarkTheme
             ? "bg-gray-800 border-red-500 shadow-red-500/50"
-            : "bg-blue-600 border-blue-400 shadow-blue-500/50"
+            : "bg-radial from-screen-blue from-35% to-screen-deep-blue border-blue-400 shadow-blue-500/50"
         }
         shadow-lg hover:shadow-xl
       `}
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.95 }}
       >
         <motion.div
           className={`
-          absolute left-[2.5px] top-0.5 flex justify-center items-center w-6 h-6 rounded-full border-2
+          absolute left-[2.1px] top-0.5 flex justify-center items-center w-6 h-6 rounded-full border-2 border-white
           transition-all duration-200
-          ${i18n.language === "en" ? "lg:left-1" : "lg:left-6.5"}
+          ${i18n.language === "en" ? "lg:left-0.5" : "lg:left-[26.4px]"}
         `}
           layout={false}
         >
@@ -37,7 +35,7 @@ export default function LangSwitch() {
             alt={i18n.language === "en" ? "English" : "Español"}
           />
         </motion.div>
-      </motion.button>
+      </button>
     </>
   );
 }
